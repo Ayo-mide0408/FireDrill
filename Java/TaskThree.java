87f7aea4b19bc31c00f810a9e7f90abe40cd00a7
Ayomide_04 <olajesuayomide04@gmail.com>
2025-06-17 17:35:55 +0100

    Add files via upload

---
+++ Java/TaskThree.java
@@ -0,0 +1,14 @@
+public class TaskThree{
+	public static void main(String[] arg){
+
+for (int count = 1; count <=10; count++){
+if (count % 2 == 1)
+System.out.print(count + " ");
+}
+
+
+
+
+}
+	
+}
